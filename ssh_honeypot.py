7257dcb8833040374982c86e8962050d46744ca7
calebtetteh <calebtetteh65@gmail.com>
2026-02-27 13:12:33 +0000

    update on emulated shell

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -1,6 +1,7 @@
 #Libraries
 import logging
 from logging.handlers import RotatingFileHandler
+import paramiko
 
 #Constants
 logging_format = logging.Formatter('%(message)s')
@@ -17,6 +18,37 @@ creds_logger.setLevel(logging.INFO)
 creds_handler = RotatingFileHandler('cmd_audits.log', maxBytes=2000, backupCount=5)
 creds_handler.setFormatter(logging_format)
 creds_logger.addHandler(creds_handler)
+
+#Emulated Shell
+def emulated_shell(channel, client_ip):
+    channel.send(b'corporate-jumpbox2$')
+    command = b""
+    while True:
+        char = channel.recv(1)
+        channel.send(char)
+        if not char:
+            channel.close()
+
+        command += char
+
+        if char == b'\r':
+            if command.strip() == b'exit':
+                response = b'\n Goodbye! \n'
+                channel.close()
+            elif command.strip() == b'pwd':
+                response = b"\n" + b'\n\\usr\\local' + b'\r\n'
+            elif command.strip() == b'whoami':
+                response = b"\n" + b"corpuser1" + b"\r\n"
+            elif command.strip() == b'ls':
+                response = b'\n' + b"jumpbox1.conf" + b"\r\n"
+            elif command.strip() == b'cat jumpbox1.conf':
+                response = b'\n' + b"Go to deebodah.com. " + b"\r\n"
+            else:
+                response = b"\n" + bytes(command.strip()) + b"\r\n"
+
+        channel.send(response)
+        channel.send(b'corporate-jumpbox2$')
+        command = b""
 
 #SSH Server + Sockets
 
