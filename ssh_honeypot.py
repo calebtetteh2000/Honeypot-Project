6f1ed7e8b42a95ecc32560db1bd7dd9ae9e1d47d
Caleb Tetteh <calebtetteh65@gmail.com>
2026-02-27 02:29:26 +0000

    Created log files

---
+++ ssh_honeypot.py
@@ -0,0 +1,22 @@
+#Libraries
+import logging
+from logging.handlers import RotatingFileHandler
+
+#Constants
+logging_format = logging.Formatter('%(message)s')
+
+#Loggers & Logging Files
+funnel_logger = logging.getLogger('FunnelLogger')
+funnel_logger.setLevel(logging.INFO)
+funnel_handler = RotatingFileHandler('audits.log', maxBytes=2000, backupCount=5)
+funnel_handler.setFormatter(logging_format)
+funnel_logger.addHandler(funnel_handler)
+
+creds_logger = logging.getLogger('FunnelLogger')
+creds_logger.setLevel(logging.INFO)
+creds_handler = RotatingFileHandler('cmd_audits.log', maxBytes=2000, backupCount=5)
+creds_handler.setFormatter(logging_format)
+creds_logger.addHandler(creds_handler)
+#SSH Server + Sockets
+
+#Provision SSH-based honeypot
